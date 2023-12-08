f7606f69c02ec8d0495e3065a50d0606ff380551
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-12-08 14:02:36 +0000

    ログイン、トップページ追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_in
   # def new
   #   super
   # end
+  def after_sign_in_path_for(public)
+     public_customers_my_page_path
+  end
+
+  def after_sign_out_path_for(public)
+     root_path
+  end
 
   # POST /resource/sign_in
   # def create
@@ -18,10 +25,11 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
 end
+
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  has_one_attached :image  
 
   enum payment_method: { credit_card: 0, pay_store: 1 }
 end
