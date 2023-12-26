a27beb64b581dddb9146d22f5a71378b92001e9b
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-12-26 14:07:31 +0000

    [finish] エラー箇所修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -17,7 +17,7 @@ class Admin::OrdersController < ApplicationController
     @order_details = @order.order_details
     @order.update(order_params)
 
-    params[:order][:cart_item].each do |item_param|
+    params[:order][:cart_item] do |item_param|
       cart_item = CartItem.find_by!(customer_id: current_customer, item_id: item.id)
       cart_item.update!(amount: item.amount)
     end
